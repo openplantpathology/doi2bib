e97a4f84f51c640cc96546027d2858c8f80b4c30
wkmor1 <wkmor1@gmail.com>
2016-03-15 23:37:10 +0000

    replace the guts of the doi2bib methods with an unexported local function

--- R/doi2bib.r
+++ R/doi2bib.r
@@ -1,7 +1,7 @@
 #' Convert doi to bibtex
 #'
 #' Convert a digital object identifier (doi) string into a bibtex entry using
-#' the webservice \link{http://www.doi2bib.org}.
+#' the webservice \url{http://www.doi2bib.org}.
 #'
 #' @param doi The character string of the doi.
 #' @param citekey Character string of equal length to \code{doi}. If used will
@@ -10,6 +10,7 @@
 #' @return a bibtex entry as a character string.
 #'
 #' @importFrom httr accept content GET
+#' @importFrom methods setGeneric setMethod
 #'
 #' @examples
 #' doi2bib("10.1038/35012251")
@@ -23,34 +24,36 @@ setGeneric(
   }
 )
 
+doi2bib_ <-
+  function(doi) {
+    vapply(
+      doi,
+      function(doi) {
+        content(
+          GET(
+            url    = "http://www.doi2bib.org/",
+            config = accept("application/x-bibtex"),
+            path   = "doi2bib",
+            query  = list(id = doi)
+          ),
+          as = "text",
+          encoding = "UTF-8"
+        )
+      },
+      character(length(doi))
+    )
+}
+
+#'@describeIn doi2bib Convert doi to bibtex
 setMethod(
   "doi2bib",
   c(doi = "character", citekey = "missing"),
   function(doi, citekey) {
-
-    refs <-
-      vapply(
-        doi,
-        function(doi) {
-          content(
-            GET(
-              url    = "http://www.doi2bib.org/",
-              config = accept("application/x-bibtex"),
-              path   = "doi2bib",
-              query  = list(id = doi)
-            ),
-            as = "text",
-            encoding = "UTF-8"
-          )
-        },
-        character(length(doi))
-      )
-
-    refs
-
+    doi2bib_(doi)
   }
 )
 
+#'@describeIn doi2bib Convert doi to bibtex
 setMethod(
   "doi2bib",
   c(doi = "character", citekey = "character"),
@@ -58,23 +61,7 @@ setMethod(
 
     stopifnot(length(citekey) == length(doi))
 
-    refs <-
-      vapply(
-        doi,
-        function(doi) {
-          content(
-            GET(
-              url    = "http://www.doi2bib.org/",
-              config = accept("application/x-bibtex"),
-              path   = "doi2bib",
-              query  = list(id = doi)
-            ),
-            as = "text",
-            encoding = "UTF-8"
-          )
-        },
-        character(length(doi))
-      )
+    refs <- doi2bib_(doi)
 
     # replace the citekeys
     mapply(
