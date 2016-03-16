a0cef7f31898ade02e3fcc67b1f3a193af7ef02c
wkmor1 <wkmor1@gmail.com>
2016-03-16 03:16:21 +0000

    overhauled the guts of doi2bib. Now accepts named arguments and can output to a file

--- R/doi2bib.r
+++ R/doi2bib.r
@@ -3,74 +3,81 @@
 #' Convert a digital object identifier (doi) string into a bibtex entry using
 #' the webservice \url{http://www.doi2bib.org}.
 #'
-#' @param doi The character string of the doi.
-#' @param citekey Character string of equal length to \code{doi}. If used will
-#'   replace citekeys with these values.
+#' @param ... One or more character strings of the doi. If arguments are named, names will replace citekeys
+#' @param file an optional character string. If used the bibtex references are sent to file rather than returned.
+#' @param quiet logical. Should the bibtex references be printed to the console?
 #'
-#' @return a bibtex entry as a character string.
+#' @return a list, returned invisibly, of bibtex references as a character strings, unless a file is specified, in which case nothing is returned
 #'
 #' @importFrom httr accept content GET
-#' @importFrom methods setGeneric setMethod
+#' @importFrom methods setGeneric setMethod signature
 #'
 #' @examples
-#' doi2bib("10.1038/35012251")
+#' doi2bib(Margules2000 = "10.1038/35012251")
 #'
 #' @export
 
 setGeneric(
   "doi2bib",
-  function(doi, citekey) {
+  function(..., file, quiet = FALSE) {
     standardGeneric("doi2bib")
-  }
+  },
+  signature = signature("...")
 )
 
-doi2bib_ <-
-  function(doi) {
-    vapply(
-      doi,
-      function(doi) {
-        content(
-          GET(
-            url    = "http://www.doi2bib.org/",
-            config = accept("application/x-bibtex"),
-            path   = "doi2bib",
-            query  = list(id = doi)
-          ),
-          as = "text",
-          encoding = "UTF-8"
+#'@describeIn doi2bib Convert doi to bibtex
+setMethod(
+  "doi2bib",
+  "character",
+  function(..., file, quiet) {
+
+    stopifnot(missing(file) || is.character(file))
+
+    stopifnot(is.logical(quiet))
+
+    dois <- c(...)
+
+    nms <- names(dois)
+
+    refs <-
+      lapply(
+        dois,
+        function(doi) {
+          content(
+            GET(
+              url    = "http://www.doi2bib.org/",
+              config = accept("application/x-bibtex"),
+              path   = "doi2bib",
+              query  = list(id = doi)
+            ),
+            as = "text",
+            encoding = "UTF-8"
+          )
+        }
+      )
+
+    if (!is.null(nms)) {
+      refs <-
+        mapply(
+          function(ref, nm) {
+            if (nchar(nm) < 1) {
+              ref
+            } else {
+              sub("([^\\{]+\\{)[^,]+", paste0("\\1", nm), ref)
+            }
+          },
+          refs,
+          nms,
+          SIMPLIFY = FALSE
         )
-      },
-      character(length(doi))
-    )
-}
+    }
 
-#'@describeIn doi2bib Convert doi to bibtex
-setMethod(
-  "doi2bib",
-  c(doi = "character", citekey = "missing"),
-  function(doi, citekey) {
-    doi2bib_(doi)
-  }
-)
+    if (!quiet) message(paste(refs, collapse = "\n"))
 
-#'@describeIn doi2bib Convert doi to bibtex
-setMethod(
-  "doi2bib",
-  c(doi = "character", citekey = "character"),
-  function(doi, citekey) {
-
-    stopifnot(length(citekey) == length(doi))
-
-    refs <- doi2bib_(doi)
-
-    # replace the citekeys
-    mapply(
-      function(refs, citekey) {
-        sub("([^\\{]+\\{)[^,]+", paste0("\\1", citekey), refs)
-      },
-      refs,
-      citekey
-    )
-
+    if (!missing(file)) {
+      cat(refs, file = file)
+    } else {
+      invisible(refs)
+    }
   }
 )
